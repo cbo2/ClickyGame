d891dc9e1e490c6b75a90e44c96b81733f3e53bd
Craig Bomba <talk2cbo@gmail.com>
2018-10-13 14:29:19 -0500

    Updates

---
+++ precache-manifest.85e61f4f82478065f234eb41e1a5b2e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49a6b76f28fdc175e78e3755cc2e868",
+    "url": "/clicky-game/static/media/Dog12.c49a6b76.jpg"
+  },
+  {
+    "revision": "e91d10a5630545a78fb7",
+    "url": "/clicky-game/static/css/main.adab3478.chunk.css"
+  },
+  {
+    "revision": "f35dc60f205875b5c0bc0b40150e98ad",
+    "url": "/clicky-game/static/media/Dog7.f35dc60f.jpg"
+  },
+  {
+    "revision": "c7132d9baa163236846d",
+    "url": "/clicky-game/static/js/1.c7132d9b.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "4b023ffb1b7ad4e5cd81d38132c64fa1",
+    "url": "/clicky-game/static/media/Dog1.4b023ffb.jpg"
+  },
+  {
+    "revision": "b02cdf2528997739c55179d9aef555a1",
+    "url": "/clicky-game/static/media/Dog11.b02cdf25.jpg"
+  },
+  {
+    "revision": "e91d10a5630545a78fb7",
+    "url": "/clicky-game/static/js/main.e91d10a5.chunk.js"
+  },
+  {
+    "revision": "8ab3baaef3aed8621f1bb5172ee7f491",
+    "url": "/clicky-game/static/media/Dog2.8ab3baae.jpg"
+  },
+  {
+    "revision": "687a4be9c57cc90942379876172aebc9",
+    "url": "/clicky-game/static/media/Dog3.687a4be9.jpg"
+  },
+  {
+    "revision": "0d0074c435e593347504c43aea8cbe34",
+    "url": "/clicky-game/static/media/Dog5.0d0074c4.jpg"
+  },
+  {
+    "revision": "e87be8b7b0586ae92e238deb6712b188",
+    "url": "/clicky-game/static/media/Dog6.e87be8b7.jpg"
+  },
+  {
+    "revision": "c7132d9baa163236846d",
+    "url": "/clicky-game/static/css/1.617bd603.chunk.css"
+  },
+  {
+    "revision": "71510cb230b24efd9406b5ab39110573",
+    "url": "/clicky-game/index.html"
+  }
+];
